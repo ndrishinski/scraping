b25f7d5f8b335de68f56d0f638c34f2fa5d25a1d
Nick <nick.drishinski@rockfordcorp.com>
2019-11-01 11:31:55 -0700

    open, click, get info

---
+++ app.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+let scrape = async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.goto('http://books.toscrape.com/');
+  await page.setViewport({width: 1000, height: 500})
+  await page.click('#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img');
+  await page.waitFor(5000);
+  // Scrape
+  const result = await page.evaluate(() => {
+    let title = document.querySelector('h1').innerText;
+    let price = document.querySelector('.price_color').innerText;
+    return {
+      title,
+      price
+    }
+    });
+  browser.close();
+  return result;
+};
+
+
+scrape().then((value) => {
+    console.log(value); // Success!
+})
+.catch(err => console.log('err', err))
