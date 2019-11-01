2d6e1bd6ff2613b864ace7adf28494c57cd08702
Nick <nick.drishinski@rockfordcorp.com>
2019-11-01 12:18:46 -0700

    get all data

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 const puppeteer = require('puppeteer');
 
 let scrape = async () => {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-  await page.goto('http://books.toscrape.com/');
-  await page.setViewport({width: 1000, height: 500})
-  await page.click('#default > div > div > div > div > section > div:nth-child(2) > ol > li:nth-child(1) > article > div.image_container > a > img');
-  await page.waitFor(5000);
-  // Scrape
-  const result = await page.evaluate(() => {
-    let title = document.querySelector('h1').innerText;
-    let price = document.querySelector('.price_color').innerText;
-    return {
-      title,
-      price
-    }
-    });
-  browser.close();
-  return result;
-};
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
 
+    await page.goto('http://books.toscrape.com/');
+    await page.setViewport({width: 1000, height: 1000})
+
+    const result = await page.evaluate(() => {
+        let data = []; // Create an empty array that will store our data
+        let elements = document.querySelectorAll('.product_pod'); // Select all Products
+        console.log('all em ', elements)
+
+        for (var element of elements){ // Loop through each proudct
+            let title = element.childNodes[5].innerText; // Select the title
+            let price = element.childNodes[7].children[0].innerText; // Select the price
+
+            data.push({title, price}); // Push an object with the data onto our array
+        }
+
+        return data; // Return our data array
+    });
+    await page.waitFor(5000);
+    browser.close();
+    return result; // Return the data
+};
 
 scrape().then((value) => {
     console.log(value); // Success!
-})
-.catch(err => console.log('err', err))
+});
